a35424d1332137715d79fefcee14a084d0f96ee6
Aaqib Mahamood <aaqibmahamood1@gmail.com>
2026-04-23 00:23:14 +0530

    chore: update lib.rs re-exports

--- shared/sim_bridge_core/src/lib.rs
+++ shared/sim_bridge_core/src/lib.rs
@@ -5,5 +5,9 @@ pub mod services;
 pub mod types;
 
 pub use bridge::{ArmMergeState, SimBridge};
-pub use config::{BridgeConfig, DaemonState};
+pub use config::{
+    read_bridge_config, read_daemon_state, resolve_joint_indices, sim_node_name, BridgeConfig,
+    DaemonState,
+};
+pub use services::{call_sim, call_sim_sync};
 pub use types::error::{BridgeError, Result};
